4e6f5f888664002068ddce3a85e2526fb4334428
Moustapha Lo <moustafalo4555@gmail.com>
2020-12-01 22:45:21 +0100

    Express REST API with Mongodb

--- app.js
+++ app.js
@@ -4,6 +4,17 @@ var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
 
+const mongoose = require('mongoose');
+
+const Dishes = require('./models/dishes');
+
+const url = "mongodb://localhost:27017/server-express";
+
+const connect = mongoose.connect(url);
+
+connect.then((db) => {
+  console.log("Connecté au serveur");
+}, (err) => {console.log(err);});
 
 var app = express();
 
