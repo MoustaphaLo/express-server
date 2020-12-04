a34b0796cbd9ecfe8ac985d9ae600531a9bf3e05
Moustapha Lo <moustafalo4555@gmail.com>
2020-12-04 10:43:00 +0100

    HTTPS

--- app.js
+++ app.js
@@ -29,6 +29,15 @@ connect.then((db) => {
 
 var app = express();
 
+app.all('*', (req, res, next) => {
+  if(req.secure) {
+    return next();
+  }
+  else {
+    res.redirect(307, 'https://'+req.hostname+':'+app.get('secPort')+req.url);
+  }
+});
+
 app.use(passport.initialize());
 app.use(passport.session());
 
